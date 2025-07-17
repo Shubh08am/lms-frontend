0a281529a569316c07674e326a6be588e286cccd
shubh08am <shubh08am@gmail.com>
2025-07-17 19:58:00 +0530

    lesson page logic

--- src/pages/LessonView.js
+++ src/pages/LessonView.js
@@ -1,3 +1,4 @@
+// In LessonView.js
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import API from "../api/api";
@@ -14,21 +15,37 @@ const LessonView = () => {
     fetchLesson();
   }, [id]);
 
+  const getEmbedUrl = (url) => {
+    try {
+      // Handle both watch?v= and youtu.be URLs
+      if (url.includes("watch?v=")) {
+        const videoId = new URL(url).searchParams.get("v");
+        return `https://www.youtube.com/embed/${videoId}`;
+      } else if (url.includes("youtu.be/")) {
+        const videoId = url.split("youtu.be/")[1];
+        return `https://www.youtube.com/embed/${videoId}`;
+      }
+    } catch {
+      return null;
+    }
+  };
+
   if (!lesson) return <div>Loading...</div>;
 
   return (
     <div>
       <h2>{lesson.title}</h2>
       <p>{lesson.content}</p>
-      {lesson.videoUrl && (
+      {lesson.videoUrl && getEmbedUrl(lesson.videoUrl) && (
         <div>
           <h4>Video:</h4>
           <iframe
             width="560"
             height="315"
-            src={lesson.videoUrl.replace("watch?v=", "embed/")}
+            src={getEmbedUrl(lesson.videoUrl)}
             title="Lesson Video"
             frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
           ></iframe>
         </div>
